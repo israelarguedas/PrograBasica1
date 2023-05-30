ead0a51e1e2f2e17ac7370791f5d8dab906da473
israelarguedas <134347102+israelarguedas@users.noreply.github.com>
2023-05-29 19:44:26 -0600

    Add files via upload

--- Ejecicio1.1.py
+++ Ejecicio1.1.py
@@ -1,1 +1,9 @@
-print("Hola Mundo")
+print ("Hola Mundo")
+
+#Datos Simples
+cadena = ("Esto es un string o cadena de caracteres") #Cadenas de caracerteres
+enteros = 20 #numeros sin decimales
+flotantes = 20.2 #numeros con decimales
+
+#Datos estructurados
+#Arreglos o vectores
---
+++ Ejecicio2.py
@@ -0,0 +1,15 @@
+#Operaciones aritmeticas
+
+resultado1 = 5 * 2 + 3
+resultado2 = 5 * (2 + 3)
+resultado3 = 5 ** 2 * 2
+resultado4 = 5 // 2 ** 3
+resultado5 = 2 % 3 - 2 
+resultado6 = 5 / 3 * 4
+
+print ("resultado 1 =", resultado1)
+print ("resultado 2 =", resultado2)
+print ("resultado 3 =", resultado3)
+print ("resultado 4 =", resultado4)
+print ("resultado 5 =", resultado5)
+print ("resultado 6 =", resultado6)
