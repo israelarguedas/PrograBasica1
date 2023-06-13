b25e388f08f13e5c245adef0971e14cb2568b76c
israelarguedas <134347102+israelarguedas@users.noreply.github.com>
2023-06-12 20:46:07 -0600

    Semana 5

---
+++ Sem5.1For.py
@@ -0,0 +1,12 @@
+#For positivo aumentando de 50 en 50
+for x in range (0,351,50):
+    print("x =", x)
+
+#For negativo disminuyendo de 2 en 2
+for i in range (400,350,-2):
+    print("i =", i)
+
+#For anidado
+for y in range (1,5):
+    for z in range (0,3):
+        print("y =", y," ","z =", z)
