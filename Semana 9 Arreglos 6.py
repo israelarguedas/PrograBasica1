3dfbd0b45b8857bb3496f1749fcff9226b350f0d
israelarguedas <134347102+israelarguedas@users.noreply.github.com>
2023-07-10 20:14:42 -0600

    Update Semana 9 Arreglos 6.py

--- Semana 9 Arreglos 6.py
+++ Semana 9 Arreglos 6.py
@@ -1,7 +1,15 @@
 arreglo = {}
 for x in range (0,10):
-    valor=int(input("Ingrese el valor:"))
-    arreglo[x] = valor
+    arreglo[x] = int(input("Ingrese el valor:"))
+    
+
+for x in range(0,len(arreglo)):
+    print("[",arreglo[x],"]",end=" ")
+
+print("\n")
+pos = int(input("Ingrese la posicion que desea modificar [0-9]: "))
+valor = int(input("Ingrese el valor:"))
+arreglo[pos] = valor
 
 for x in range(0,len(arreglo)):
     print("[",arreglo[x],"]",end=" ")
